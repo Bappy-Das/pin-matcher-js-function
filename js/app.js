05fbf5ea0c3704adf7ba77073d3ad4730093c9d7
Bappy-Das <bappydas.bd51@gmail.com>
2021-08-19 03:09:49 +0600

    added checking by click submit

--- js/app.js
+++ js/app.js
@@ -1,43 +1,51 @@
-// function getPin() {
-//     const generate = Math.round(Math.random() * 10000);
-//     const pinString = generate + '';
-//     if (pinString.length == 4) {
-//         return pinString;
-//     }
-//     else {
-//         return getPin();
-//     }
-// }
+function getPin() {
+    const generate = Math.round(Math.random() * 10000);
+    const pinString = generate + '';
+    if (pinString.length == 4) {
+        return pinString;
+    }
+    else {
+        return getPin();
+    }
+}
 
-// function generatePin() {
-//     const pin = getPin();
-//     document.getElementById("pinId").value = pin;
-// }
+function generatePin() {
+    const pin = getPin();
+    document.getElementById("pinId").value = pin;
+}
 
-// document.getElementById('key-pad').addEventListener('click', function (event) {
-//     const number = event.target.innerText;
-//     const calc = document.getElementById('typed-numbers');
-//     if (isNaN(number)) {
-//         if (number == 'C') {
-//             calc.value = '';
-//         }
-//     } else {
-//         const previousCalc = calc.value;
-//         const newCalc = previousCalc + number;
-//         calc.value = newCalc;
-//     }
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calc = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calc.value = '';
+        }
+    } else {
+        const previousCalc = calc.value;
+        const newCalc = previousCalc + number;
+        calc.value = newCalc;
+    }
 
-// })
-
-
-// let a = Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9);
-// console.log(a);
+})
 
 
+function checkPin() {
+    const generatePin = document.getElementById('pinId').value;
+    const currentPin = document.getElementById('typed-numbers').value;
+    const errorPin = document.getElementById('error-pin');
+    const succesPin = document.getElementById('succes-pin');
+    if (generatePin == currentPin) {
+        succesPin.style.display = "block";
+        errorPin.style.display = 'none';
+    }
+    else {
+        succesPin.style.display = "none";
+        errorPin.style.display = 'block';
+    }
+}
 
-const category = 'pen';
-const performer = category + '-teller';
-console.log(performer);
+
 
 
 
